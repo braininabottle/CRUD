ca5bd353ae9edcafd0858c7a435b8add5b19a7ba
Juan Pablo Lagos Moreno <juan.lagos.moreno91@gmail.com>
2022-07-16 20:39:15 -0400

    Modificaciones a la validacion del formulario

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,55 +2,39 @@
 // formularioEnLaPagina = formulario que se ve por la parte del usuario.
 // ListaHerramientasEnLaPagina = Lista de herramientas que se va a ver por la parte del usuario.
 // arrayDeHerramientas = array que va almacenando las herramientas que se van creando.
-
 const formularioEnLaPagina = document.getElementById("formulario");
 const listaHerramientasEnLaPagina = document.getElementById("listaHerramientas");
 let arrayDeHerramientas = []; 
-let modoEdicion = false
+let modoEdicion = false //modo edición siempre va a comnenzar en falso porque debe obedecer a una condición de un evento.
 //funciones
-
 //Función CREATE, crea la herramienta y luego le da un push al array de herramientas, que inicialmente esta vacío.
-
 const crearHerramienta = herramienta => {
-    
-    let item = 
+    if(formulario.value === "" ){
+        window.alert('debe ingresar nombre de alguna herramienta')
+    }else{
+        let item = 
         {
             herramienta: herramienta, 
-            estado: false
         }
-   
         let elementoYaExiste = arrayDeHerramientas.find((element) => element.herramienta === herramienta)
-        
         if(elementoYaExiste !== undefined){
            window.alert('Esta herramienta ya se encuentra en esta lista')
         }else{
            arrayDeHerramientas.push(item);
         }
-        
     return item;
-
 }
-
-
+}
 // función que  almacena los  datos en el local Storage para que no se pierda la información en caso de actualizar la página. 
 //Solo desaparece la info cuando usuario elimiina esta misma.
-
 const almacenarDatosLS = () => {
-
     localStorage.setItem('compras', JSON.stringify(arrayDeHerramientas)); //JSON.STRINGIFY convierte el array a un string ya que setItem() solo recibe valores de string.
-
     TrasladarDatosAlmacenadosEnLSaSitioWeb();
 }
-
-
 // Función READ, crea el elemento de manera dinamica.
-
 const TrasladarDatosAlmacenadosEnLSaSitioWeb = () => {
-
     listaHerramientasEnLaPagina.innerHTML = '';
-    
     arrayDeHerramientas=JSON.parse(localStorage.getItem('compras'));
-    
     if(arrayDeHerramientas === null){
         arrayDeHerramientas = [];
     }else{
@@ -58,53 +42,26 @@ const TrasladarDatosAlmacenadosEnLSaSitioWeb = () => {
             listaHerramientasEnLaPagina.innerHTML += `<div class="alert alert-secondary" role="alert">
             <i class="icono-texto-alerta material-icons">home_repair_service</i>
             <b class="texto-herramienta">${herramienta.herramienta}</b>
-            <span class="contenedor-icono-editar-borrar"><i class="material-icons">drive_file_rename_outline</i>
-            <i class="material-icons">delete_sweep</i></span></div>`
+            <span class="contenedor-icono-editar-borrar"><i class="material-icons" id=${herramienta.herramienta}>drive_file_rename_outline</i>
+            <i class="material-icons" id=${herramienta.herramienta}>delete_sweep</i></span></div>`
         });
-       
-
     }
-
 }
-
 //función eliminar herramienta
-
 const eliminarHerramienta = herramienta => {
-    
-    let indexArray;
-
-   arrayDeHerramientas.map((element, index) => {
-        
-    if(element.herramienta === herramienta){
-         
-        indexArray = index;
-
-    }
-
-    
-   
-});
-
-arrayDeHerramientas.splice(indexArray, 1);
-
+let indiceArray = arrayDeHerramientas.findIndex((element) => element.herramienta === herramienta)
+arrayDeHerramientas.splice(indiceArray, 1);
 almacenarDatosLS();
-
 } 
-
 //editar herramienta 
-
 const editarHerramienta = herramienta => {
-   
     modoEdicion = true;
     document.getElementById("boton-multi-funcion").innerText = 'Confirmar'
     let herramientaEscritaUsuario = document.getElementById("herramienta");
     herramientaEscritaUsuario.value = herramienta
     herramientaEscritaUsuario.setAttribute('data-antiguo',herramienta)
-
 }
-
 //función guardar cambios
-
 const guardarCambios = () => {
     let herramientaEscritaUsuario = document.getElementById("herramienta");
     let indexArray = arrayDeHerramientas.findIndex((element) => element.herramienta === herramientaEscritaUsuario.getAttribute('data-antiguo') );
@@ -113,13 +70,10 @@ const guardarCambios = () => {
     modoEdicion = false
     document.getElementById("boton-multi-funcion").innerText = 'Agregar'
 }
-
 //EventListener 
 //evento= se crea para prevenir que la página se refresque 
 //herramientaEscritaUsuario = Cuando el usuario ingresa una herramienta en el formulario.
-
 formularioEnLaPagina.addEventListener('submit', (evento) => {
-    
     evento.preventDefault();
     let herramientaEscritaUsuario = document.getElementById("herramienta").value; 
     if(modoEdicion === true ){
@@ -127,35 +81,20 @@ formularioEnLaPagina.addEventListener('submit', (evento) => {
     }else {
         crearHerramienta(herramientaEscritaUsuario);
     }
-    
     almacenarDatosLS();
-
     formularioEnLaPagina.reset();
-
 })
-
 document.addEventListener('DOMContentLoaded', TrasladarDatosAlmacenadosEnLSaSitioWeb);
-
 listaHerramientasEnLaPagina.addEventListener('click', (event) => {
-    
     event.preventDefault();
-     
     if(event.target.innerHTML === 'drive_file_rename_outline' || event.target.innerHTML === 'delete_sweep'){ 
-        
-        const rutadeEliminacion = event.path[2].childNodes[3].innerHTML;
-
+        let rutaIconos = event.target.id;
         if(event.target.innerHTML === 'delete_sweep'){
-           
-            eliminarHerramienta(rutadeEliminacion);
-
+            eliminarHerramienta(rutaIconos);
         }
         if(event.target.innerHTML === 'drive_file_rename_outline'){
-
-            editarHerramienta(rutadeEliminacion);
+            editarHerramienta(rutaIconos);
         }
-    
     }
-      
-    
 });
 
